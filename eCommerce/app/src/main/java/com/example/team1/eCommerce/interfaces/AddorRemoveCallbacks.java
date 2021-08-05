3adba3256bdd48e1c0ae3dacf898009f2b011816
Shakyajyoti <pyneshakyajyoti007@gmail.com>
2021-08-05 20:16:48 +0530

    first commit

---
+++ eCommerce/app/src/main/java/com/example/team1/eCommerce/interfaces/AddorRemoveCallbacks.java
@@ -0,0 +1,13 @@
+package com.example.team1.eCommerce.interfaces;
+
+
+
+
+public interface AddorRemoveCallbacks {
+
+    void onAddProduct();
+
+    void onRemoveProduct();
+
+    void updateTotalPrice();
+}
